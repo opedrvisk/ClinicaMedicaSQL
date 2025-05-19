4bd08f82e764e0ca399bcec4d21bcab1c284ec32
pedrovisk <pedro.h.marcondes.rocha@gmail.com>
2025-05-19 19:24:33 -0300

    Código do sistema

---
+++ Consulta.cs
@@ -0,0 +1,89 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+public class Consulta
+{
+    public int PacienteId { get; set; }
+    public int MedicoId { get; set; }
+    public DateTime DataHora { get; set; }
+    public Consulta(int pacienteId, int medicoId, DateTime dataHora)
+    {
+    PacienteId = pacienteId;
+    MedicoId = medicoId;
+    DataHora = dataHora;
+}
+public void Salvar()
+{
+    using (MySqlConnection con = Conexao.ObterConexao())
+    {
+        MySqlCommand cmd = new MySqlCommand("INSERT INTO Consulta (paciente_id, medico_id, data_hora) VALUES (@pid, @mid, @data)", con);
+        cmd.Parameters.AddWithValue("@pid", PacienteId);
+        cmd.Parameters.AddWithValue("@mid", MedicoId);
+        cmd.Parameters.AddWithValue("@data", DataHora);
+        cmd.ExecuteNonQuery();
+    }
+}
+    public static void ListarTodas()
+    {
+        using (MySqlConnection con = Conexao.ObterConexao())
+        {
+            MySqlCommand cmd = new MySqlCommand(@"
+            SELECT c.id, p.nome AS paciente, m.nome AS medico, c.data_hora
+            FROM Consulta c
+            JOIN Paciente p ON c.paciente_id = p.id
+            JOIN Medico m ON c.medico_id = m.id
+            ORDER BY c.data_hora", con);
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                Console.WriteLine("\n--- Lista de Consultas ---");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["id"]}, Paciente: {reader["paciente"]}, Médico: {reader["medico"]}, Data: {reader["data_hora"]}");
+                }
+            }
+        }
+    }
+     public static void ConsultarPorId(int id)
+     {
+        using (MySqlConnection con = Conexao.ObterConexao())
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Consulta WHERE id = @id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["id"]}, PacienteID: {reader["paciente_id"]}, MedicoID: {reader["medico_id"]}, DataHora: {reader["data_hora"]}");
+                }
+                else
+                {
+                    Console.WriteLine("Consulta não encontrada.");
+                }
+            }
+        }
+    }
+    public static void Deletar(int id)
+    {
+        using (MySqlConnection con = Conexao.ObterConexao())
+        {
+            try
+            {
+                MySqlCommand deletarCmd = new MySqlCommand("DELETE FROM Consulta WHERE id = @id", con);
+                deletarCmd.Parameters.AddWithValue("@id", id);
+                int linhasAfetadas = deletarCmd.ExecuteNonQuery();
+
+                if (linhasAfetadas > 0)
+                {
+                    Console.WriteLine("Consulta deletada com sucesso.");
+                }
+                else
+                {
+                    Console.WriteLine("Consulta não encontrada.");
+                }
+            }
+            catch (MySqlException ex)
+            {              
+                    Console.WriteLine($"Erro ao tentar deletar a consulta: {ex.Message}");
+                }
+            }
+        }
+     }
---
+++ Medico.cs
@@ -0,0 +1,97 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+
+public class Medico : Pessoa
+{
+    public string Especialidade { get; set; }
+
+    public Medico(string nome, string cpf, string especialidade)
+        : base(nome, cpf)
+    {
+        Especialidade = especialidade;
+    }
+
+    public override void Salvar()
+    {
+        using (MySqlConnection con = Conexao.ObterConexao())
+        {
+            MySqlCommand cmd = new MySqlCommand("INSERT INTO Medico (nome, cpf, especialidade) VALUES (@nome, @cpf, @especialidade)", con);
+            cmd.Parameters.AddWithValue("@nome", Nome);
+            cmd.Parameters.AddWithValue("@cpf", CPF);
+            cmd.Parameters.AddWithValue("@especialidade", Especialidade);
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    public override void MostrarInfo()
+    {
+        Console.WriteLine($"Dr. {Nome}, Especialidade: {Especialidade}");
+    }
+
+    public static void ListarTodos()
+    {
+        using (MySqlConnection con = Conexao.ObterConexao())
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Medico", con);
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                Console.WriteLine("\n--- Lista de Médicos ---");
+                while (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["id"]}, Nome: {reader["nome"]}, CPF: {reader["cpf"]}, Especialidade: {reader["especialidade"]}");
+                }
+            }
+        }
+    }
+    public static void ConsultarPorId(int id)
+    {
+        using (MySqlConnection con = Conexao.ObterConexao())
+        {
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM Medico WHERE id = @id", con);
+            cmd.Parameters.AddWithValue("@id", id);
+            using (MySqlDataReader reader = cmd.ExecuteReader())
+            {
+                if (reader.Read())
+                {
+                    Console.WriteLine($"ID: {reader["id"]}, Nome: {reader["nome"]}, CPF: {reader["cpf"]}, Especialidade: {reader["especialidade"]} ");
+                }
+                else
+                {
+                    Console.WriteLine("Medico não encontrado.");
+                }
+            }
+        }
+    }
+    public static void Deletar(int id)
+    {
+        using (MySqlConnection con = Conexao.ObterConexao())
+        {
+            try
+            {
+                MySqlCommand deletarCmd = new MySqlCommand("DELETE FROM Medico WHERE id = @id", con);
+                deletarCmd.Parameters.AddWithValue("@id", id);
+                int linhasAfetadas = deletarCmd.ExecuteNonQuery();
+
+                if (linhasAfetadas > 0)
+                {
+                    Console.WriteLine("Médico deletado com sucesso.");
+                }
+                else
+                {
+                    Console.WriteLine("Médico não encontrado.");
+                }
+            }
+            catch (MySqlException ex)
+            {
+                if (ex.Number == 1451) // erro de chave estrangeira
+                {
+                    Console.WriteLine("Não é possível deletar o médico porque ele possui consultas associadas.");
+                }
+                else
+                {
+                    Console.WriteLine($"Erro ao tentar deletar o médico: {ex.Message}");
+                }
+            }
+        }
+    }
+}
